fcb7ebe1ec9e5c1a4d90b26e1b2f60e610c7eb2e
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-27 19:16:56 +0530

    eventmodifiers

---
+++ vuejs/src/components/eventmodifiers.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>eventmodifiers</h1>
+        <button @click="increase(5)">clickonit</button>
+        <p>{{count}}</p>
+     <!--   <p v-on:mousemove="updatecoordinates">coordinates:{{x}}/{{y}}
+            -<span v-on:mousemove="dummy">DEAD SPOT</span </p> -->
+            <p v-on:mousemove="updatecoordinates">coordinates:{{x}}/{{y}}
+            -<span v-on:mousemove.stop="">DEAD SPOT</span> </p>
+        
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"ModifierS",
+    data(){
+        return{
+            count:0,
+            x:0,
+            y:0
+        }
+    },
+    methods:{
+        increase(step){
+            this.count+=step
+        },
+        updatecoordinates(event){
+            this.x=event.clientX ///NOTE HERE WILL BE X AND Y SHOULD IN CAPITAL SO IT WILL WORK
+            this.y=event.clientY
+        }
+       // dummy(event){
+     //   event.stopPropagation()      NOTE:THIS IS USE WHEN WE ARE USING FUNTION LIKE DUMMY
+      //  }
+
+    }
+}
+</script>
