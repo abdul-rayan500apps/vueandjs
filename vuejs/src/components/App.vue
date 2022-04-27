75e0f4860b23dea2a544a9711e0344bcba0e9327
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-27 19:18:53 +0530

    app

---
+++ vuejs/src/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+    <input type="text" v-on:input=changetitle>
+    <button @click="changetitle">submit</button>
+        <p>{{title}}</p>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"ApP",
+    data(){
+        return{
+        title:"hello world"
+        }
+         },
+        methods:{
+            changetitle:function(event){
+                this.title=event.target.value;
+            }
+           
+    }
+}
+</script>
