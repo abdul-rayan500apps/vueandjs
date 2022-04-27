a1c74abb746af5bbfc097249b7b415096ff24903
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-27 19:17:19 +0530

    keymodifiers

---
+++ vuejs/src/components/keymodifiers.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>key modifiers</h1>
+        <input type="text" v-on:keyup="alertme">  <!--we can use more properties like v-on:keyup.enter.space {{if we give enter and space it wil alert}}-->
+    </div>
+</template>
+<script>
+
+
+export default{
+    name:"KeyModifiers",
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        alertme(){
+        alert("alert!")
+        }
+    }
+}
+</script>
