19a69757c90870c78e90ec9961ddfdd2d70e9fed
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-27 19:16:45 +0530

    event

---
+++ vuejs/src/components/event.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>event directive</h1>
+        <button v-on:click="increase">click</button>
+        <p>{{count}}</p>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"EvenT",
+    data(){
+        return{
+            count:0
+        }
+    },
+    methods:{
+        increase:function(){
+          this.count++
+        }
+    }
+}
+</script>
