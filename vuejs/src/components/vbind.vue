6443826fd857c652b44383b3bdebf5137bd65605
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-27 19:18:32 +0530

    vbind

---
+++ vuejs/src/components/vbind.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>v-bind attributes</h1>
+        
+         <!--  <p>{{ sayhello() }} -<a href= "{{link}}">Google></a></p>  it will wont work-->
+        <p>{{ sayhello() }} -<a v-bind:href="link">Google</a></p>
+     
+        
+       
+</div>
+</template>
+<script>
+
+
+export default {
+    name:"VbinD",
+    data(){
+        return{
+            title:"hello",
+            link:'http://google.com'
+        }
+    },
+    methods:{
+        sayhello:function() {
+            return this.title
+        }
+    }
+}
+</script>
