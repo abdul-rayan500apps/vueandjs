1878ac506db378d03fdb9a9328d04f8a7fb3b4f8
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-27 19:18:06 +0530

    vueinstance

---
+++ vuejs/src/components/vueinstance.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>vue instance</h1>
+        <p>{{sayhello()}}</p>
+    </div>
+</template>
+<script>
+
+
+export default{
+    name:"InstancE",
+    data(){
+        return{
+           title:"Hello World!"
+        }
+        
+    },
+    methods:{
+      sayhello:function(){
+          return this.title;
+      }
+    }
+}
+</script>
