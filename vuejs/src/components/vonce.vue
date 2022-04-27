3490e4b9d9ae365dd94581bf6f9de141acee639d
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-27 19:18:40 +0530

    vonce

---
+++ vuejs/src/components/vonce.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p v-once >{{title}}</p>
+         <p>{{ sayhello() }} -<a v-bind:href="link">Google</a></p>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"OncE",
+    data(){
+        return{
+            title:"hello world!",
+            link:'http://google.com'
+            
+        }
+    },
+    methods:{
+        sayhello:function(){
+            this.title="hello"
+            return this.title
+        }
+    }
+}
+</script>
