42f2e89bcd94ea97364385731a23b8d4305afd30
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-27 19:17:33 +0530

    vhtml

---
+++ vuejs/src/components/vhtml.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>vue-html</h1>
+        <p>{{title}}</p>
+        <p>{{sayhello()}} -<a href="link">Google</a></p>
+      <!--  <p>{{finishedlink}}</p> it will work --> 
+      <p v-html="finishedlink"></p>
+
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"VhtmL",
+    data(){
+        return{
+           title:"hello world", 
+           link:"http://google.com",
+           finishedlink:'<a href="http://google.com">Google</a>'       
+        }
+    },
+    methods:{
+        sayhello:function(){
+            this.title="hello!"
+            return this.title
+        }
+    }
+}
+</script>
