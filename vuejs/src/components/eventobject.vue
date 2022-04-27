8bf05495ddb845ce37e3de644008ed56f914d526
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-27 19:18:17 +0530

    eventobject

---
+++ vuejs/src/components/eventobject.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>eventobject</h1>
+        <!--<button @click="increase()">clickonit</button> -->
+        <button @click="increase(2,$event)">clickonit</button>
+        <p>{{count}}</p>
+        <p v-on:mousemove="updatecoordinates">updatecoordinates:{{x}}/{{y}}</p>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"ObjecT",
+    data(){
+        return{
+            count:0,
+            x:0,
+            y:0
+        }
+        },
+    
+    methods:{
+        increase(step){
+           // this.count++    
+            this.count+=step   //THIS IS WITH ARGUMENTS
+    },
+     updatecoordinates(event){
+        this.x = event.clientX;
+        this.y = event.clientY
+    }
+   
+}
+}
+</script>
