fbe90b6b0f099e44104061e65f7ca50bfd34cac2
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-27 19:17:08 +0530

    jsexpression

---
+++ vuejs/src/components/jsexpression.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>js expressions</h1>
+        <button @click="increase(2)">clickonit</button>
+        <button @click="count++">clickonit</button>
+        <p>{{count > 10 ? 'greater than 10':'less than 10'}}</p>
+    </div>
+</template>
+<script>
+
+
+export default{
+    name:"JsexpressioN",
+    data(){
+        return{
+            count:0
+        }
+    },
+    methods:{
+        increase(step){
+            this.count+=step
+        }
+    }
+}
+</script>
