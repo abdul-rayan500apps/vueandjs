38537c11caa1b2174c8d00b050b1500bdcd629e6
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-27 19:17:53 +0530

    twowaybinding

---
+++ vuejs/src/components/twowaybinding.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>two way binding</h1>
+        <input type="text" v-model="name">
+        <p>{{name}}</p>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"TwoBinding",
+    data(){
+        return{
+            name:""
+        }
+    }
+}
+</script>
